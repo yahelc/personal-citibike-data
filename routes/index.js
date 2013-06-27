cc25cb9802ecf909f72460b2036543b63743ef2b
Yahel Carmon <yahelc@gmail.com>
2013-06-27 02:00:19 -0400

    Add DOM parsing and output logic

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@
 
 exports.index = function(req, res){
 	var request = require('request');
+	var $ = require("jquery");
 	
 	request('https://citibikenyc.com/login', function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
-	    console.log(response.headers["set-cookie"]) // Print the google web page.
-		var csrf = response.headers["set-cookie"][2].split("=")[1].split("; ")[0];
+		var csrf = response.headers["set-cookie"][2].split("=")[1].split("; ")[0]; //y u no do smart
 		var po = request.post('https://citibikenyc.com/login', {form:
 			{'subscriberUsername': '',
 			'login_submit':'Login',		
@@ -17,13 +17,19 @@ exports.index = function(req, res){
 			"ci_csrf_token": csrf
 		}}, function(error, response, body){
 			var r = request("https://citibikenyc.com/member/trips", function (error, response, body){
-			//	console.log(body);
-				var htmlparser = require("htmlparser");
-				var handler = new htmlparser.DefaultHandler(function (error, dom) {
+				var json = [];
+				var keys = $("th", body).map(function(i,v){return $(v).html();}).get();
+				$("tr", body).each(function(i,val){
+					var trip = {};
+					$("td", $(val, body)).each(function(k,v){
+						trip[ keys[k]  ] = $(v).text();
+						})
+					json.push(trip);
 				});
-				var parser = new htmlparser.Parser(handler);
-				parser.parseComplete(body);
-				console.log(handler.dom);
+				
+			 res.json(json);
+				
+				
 			});
 			console.log(r);
 		}
@@ -35,5 +41,5 @@ exports.index = function(req, res){
 	
 /*	
 */
-  res.render('index', { title: 'Express' });
+ // res.render('index', { title: 'Express' });
 };
